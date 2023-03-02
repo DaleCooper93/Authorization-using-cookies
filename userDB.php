07aa0f0435098c75e7c394dac2c1cdfb19ecdc7a
sergei <s.y.shtanko@yandex.ru>
2023-03-02 13:58:40 +0500

    Очередное изменение проекта

---
+++ userDB.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    ['login' => 'admin', 'password' => 'password'],
+    ['login' => 'moderator', 'password' => '123'],
+    ['login' => 'manynewstrap93', 'password' => 'bumer21293']
+];
+
